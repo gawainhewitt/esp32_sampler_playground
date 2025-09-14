f93c80834a8737fa04bfccf761436d795cbb9040
Gawain Hewitt <gawainhewitt@02c26e4c-1b5e-4b5a-9b6f-78dc24815a45.fritz.box>
2025-09-14 07:12:48 +0100

    wired for either SPI or 4 bit sd card

--- src/config.h
+++ src/config.h
@@ -10,23 +10,31 @@
 #define MAX_INSTRUMENTS       4           // Maximum number of instruments
 
 // I2S pins for UDA1334A DAC
-#define I2S_BCLK_PIN    42
-#define I2S_DOUT_PIN    41  
-#define I2S_WCLK_PIN    2
+#define I2S_BCLK_PIN    5
+#define I2S_DOUT_PIN    6  
+#define I2S_WCLK_PIN    7
 
 // SD Card SPI pins
-#define SD_CS_PIN    10
-#define SD_MOSI_PIN  11
-#define SD_MISO_PIN  13
-#define SD_SCK_PIN   12
+#define SD_CS_PIN    12
+#define SD_MOSI_PIN  3
+#define SD_MISO_PIN  9
+#define SD_SCK_PIN   46
+
+// SD Card 4 bit pins:
+// #define SDMMC_CMD 3
+// #define SDMMC_CLK 46 
+// #define SDMMC_D0  9
+// #define SDMMC_D1  10
+// #define SDMMC_D2  11
+// #define SDMMC_D3  12
 
 // MIDI pins
 #define MIDIRX_PIN      4
 #define MIDITX_PIN      9
 
 // Audio settings
-#define DMA_BUF_LEN     64
-#define DMA_NUM_BUF     2
+#define DMA_BUF_LEN     256
+#define DMA_NUM_BUF     8
 
 // Global audio settings
 extern float sampleVolume;
